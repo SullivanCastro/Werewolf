3493c09046398445ce96d16ac05c048869436905
SullivanCastro <sullivan.castro@eleves.enpc.fr>
2025-03-27 16:55:50 +0100

    Retake

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 class Agent:
-    def __init__(self, is_leader, is_avenger):
-        self.is_leader = is_leader
-        self.is_avenger = is_avenger
-        self.suspect_werewolf = None
-        self.last_attacker = None
+    def __init__(self, id, game, is_werewolf=False):
+        self.beliefs = np.ones(game.num_players)
+        self.beliefs[id] = 0
+        if is_werewolf:
+            self.beliefs[game.werewolfs_id] = 0
+        self.beliefs /= np.sum(self.beliefs)
 
     def vote(self, suspects):
         # Draw randomly in the list of suspect
         return np.random.choice(suspects)
+    
+    
 
--- src/agent/villagers.py
+++ src/agent/villagers.py
@@ -3,15 +3,15 @@ import numpy as np
 from .agent import Agent
 
 class Villager(Agent):
-    def __init__(self, name, is_leader, is_avenger):
-        super().__init__(is_leader, is_avenger)
+    def __init__(self, name, game):
+        super().__init__()
         self.name = name
         self.type = "Villager"
+        self.beliefs = np.ones(game.num_players)
+        self.beliefs[game.player_id] = 0
 
-    def _draw_suspect(self, suspects, game):
-        if self.is_avenger and game.is_alive(self.last_attacker):
-            return np.arange(len(suspects)) == self.last_attacker
-        return suspects
+    def _draw_suspect(self):
+        return np.argmax(self.beliefs)
     
     def choose_vote(self, suspects):
         return self.vote(self._draw_suspect(suspects))
